e7974fc5c14e293680fc194470827f71892f00c0
jjajaap <117694668+jjajaap@users.noreply.github.com>
2023-03-24 10:36:10 +0700

    Add files via upload

---
+++ PRAK ALPRO PT 4/FrekNilTabel.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int N[10]= {7,4,5,7,6,5,3,5,1,4};
+    int elmt;
+    int i, j;
+
+    for (i = 0; i < 10; i++){
+        elmt = 1;
+        for ( j = i + 1; j < 10; j++){
+            if (N[i] != 0 && N[i] == N[j]){
+                elmt = elmt + 1;
+                N[j] = 0;
+            }
+        }
+        if (elmt > 1){
+            printf("%d", N[i]);
+        }
+    }
+}
---
+++ PRAK ALPRO PT 4/NilMax2Tabel.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int N[10]= {7,4,5,7,6,5,3,5,1,4};
+    int i;
+    int max1, max2;
+    //scanf("%d", &N, &T);
+
+    max1 = 0;
+    max2 = 0;
+
+    for (i = 0; i <= 10; i++){
+        if (max1 < N[i]){
+            max2 = max1;
+            max1 = N[i];
+        }
+        if (max2 < N[i] && max1 != N[i]){
+            max2 = N[i];
+        }
+    }
+    printf("%d", max2);
+
+    return 0;
+}
